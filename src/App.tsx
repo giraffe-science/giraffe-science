d615888a858fdcae82bb4b3e084dbf82a122dabc
Matthew Savage <savagematt@users.noreply.github.com>
2020-08-11 10:39:22 +0100

    less fur

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export function App({loading, lookup}: { loading: Promise<Library>, lookup: Look
         console.log(error);
 
 
-    const furHeight = "64px";
+    const furHeight = "62px";
     const furMargin = "8px";
     return (
         <ThemeProvider theme={theme}>
