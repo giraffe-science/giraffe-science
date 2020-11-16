71905ab43ccdb185598bbba8a9ee2fdbbfeef8d2
scientific-giraffe <scientific-giraffe@users.noreply.github.com>
2020-11-16 20:32:34 +0000

    deploy: 6a168aaf00c88040519e3592e2ee2a957323e690

---
+++ precache-manifest.67cf1e36107558ad76b963d730bc7a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f196aa00ca0c4ed74a605fe33fd7723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ad963b3ae9806d92d6",
+    "url": "/static/css/main.a8750211.chunk.css"
+  },
+  {
+    "revision": "bda9ef8ca7747ace05ab",
+    "url": "/static/js/2.03fa526e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.03fa526e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ad963b3ae9806d92d6",
+    "url": "/static/js/main.37415bd0.chunk.js"
+  },
+  {
+    "revision": "16967a5a6256b9c3c70f",
+    "url": "/static/js/runtime-main.ce6f8cd9.js"
+  },
+  {
+    "revision": "3f8525c8a63ab755d53f5ae5ebfe499d",
+    "url": "/static/media/fur-2.3f8525c8.png"
+  },
+  {
+    "revision": "4036044153526d741e28b517aeac9021",
+    "url": "/static/media/fur.40360441.png"
+  }
+]);
