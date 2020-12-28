50c2efa7e40e2f716a32aa2f2a24688c8272d546
scientific-giraffe <github@giraffe.science>
2020-12-28 10:34:47 +0000

    Receiving email to transactional email subdomain

--- deploy/lib/ses-stack.ts
+++ deploy/lib/ses-stack.ts
@@ -1,21 +1,25 @@
+import * as route53 from "@aws-cdk/aws-route53";
 import {PublicHostedZone} from "@aws-cdk/aws-route53";
 import * as s3 from "@aws-cdk/aws-s3";
 import {BucketAccessControl, BucketEncryption} from "@aws-cdk/aws-s3";
 import * as ses from "@aws-cdk/aws-ses";
 import * as sesActions from '@aws-cdk/aws-ses-actions';
 import * as cdk from "@aws-cdk/core";
-import {StackProps} from "@aws-cdk/core";
+import {Duration, StackProps} from "@aws-cdk/core";
 import * as magic from 'aws-cdk-ses-domain-identity'
 import 'source-map-support/register';
 
 export class SesStack extends cdk.Stack {
     readonly transactionalMailSesSubdomain: magic.DnsValidatedDomainIdentity;
 
-    constructor(scope: cdk.Construct, id: string, env:string,zone: PublicHostedZone, props: StackProps) {
+    constructor(scope: cdk.Construct, id: string, env: string, zone: PublicHostedZone, props: StackProps) {
         super(scope, id, {
             description: "Scientific Giraffe SES",
             ...props
         });
+        // We use a subdomain so that transactional emails do not affect the reputation of our main domain
+        const transactionalEmailSubdomain = `hello.${zone.zoneName}`;
+        const signupEmailAddress = `signup@${transactionalEmailSubdomain}`;
 
         // TODO: do we actually need this?
         new magic.DnsValidatedDomainIdentity(this, "sesDomainIdentity", {
@@ -24,10 +28,9 @@ export class SesStack extends cdk.Stack {
             dkim: true,
             hostedZone: zone
         });
-        // We use a subdomain so that transactional emails do not affect the reputation of our main domain
         this.transactionalMailSesSubdomain = new magic.DnsValidatedDomainIdentity(this, "sesDomainIdentitySignup", {
             region: props.env?.region,
-            domainName: `hello.${zone.zoneName}`,
+            domainName: transactionalEmailSubdomain,
             dkim: true,
             hostedZone: zone
         });
@@ -55,19 +58,31 @@ export class SesStack extends cdk.Stack {
             accessControl: BucketAccessControl.PRIVATE,
             encryption: BucketEncryption.S3_MANAGED,
         });
-        const signupRuleSet = `signup-${env}`;
         new ses.ReceiptRuleSet(this, "signupReceiptRuleSet", {
-            receiptRuleSetName: signupRuleSet,
+            receiptRuleSetName: `signup-${env}`,
+            dropSpam: true,
             rules: [
                 {
                     receiptRuleName: `sg-signup-s3-${env}`,
+                    recipients: [signupEmailAddress],
                     actions: [
                         new sesActions.S3({
-                            bucket:signupRepliesBucket
+                            bucket: signupRepliesBucket,
+                            objectKeyPrefix: "signup-"
                         })
                     ]
                 }
             ]
         });
+        // https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-mx-record.html
+        new route53.MxRecord(this, "transactionalMxRecord", {
+            zone,
+            recordName: transactionalEmailSubdomain,
+            ttl: Duration.minutes(5),
+            values: [{
+                priority: 10,
+                hostName: `inbound-smtp.${props.env?.region}.amazonaws.com`
+            }]
+        })
     }
 }
